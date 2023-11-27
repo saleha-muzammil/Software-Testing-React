eb6ad070528d3e4a29d2e810a9bb31cdca71b673
Saleha Muzammil <84681153+saleha-muzammil@users.noreply.github.com>
2023-11-27 23:45:33 +0500

    Update firebase.js

--- Project/src/Firebase/firebase.js
+++ Project/src/Firebase/firebase.js
@@ -7,22 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
 
-  apiKey: "AIzaSyB3W1QSoUA4czhze15zM3QJrWbfZnuxOEU",
-
-  authDomain: "stproject-161c6.firebaseapp.com",
-
-  projectId: "stproject-161c6",
-
-  storageBucket: "stproject-161c6.appspot.com",
-
-  messagingSenderId: "1054039637485",
-
-  appId: "1:1054039637485:web:d1ca1fb8cffcc78b3a3be4",
-
-  measurementId: "G-6FJRP200Q7"
-
-
-
+  apiKey: "your-key",
+  authDomain: "your-link",
+  projectId: "your-id",
+  storageBucket: "your-bucket-link",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id",
+  measurementId: "your-measurement-id"
 };
 
 // Initialize Firebase
